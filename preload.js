88c1440f98ccb468b4a246e672915c44934d708b
dieutrinh <56671269+dieutrinh@users.noreply.github.com>
2025-12-23 21:06:10 +0700

    Update preload.js

--- preload.js
+++ preload.js
@@ -4,26 +4,25 @@ const QRCode = require("qrcode");
 contextBridge.exposeInMainWorld("qrFactory", {
   // tạo QR dataURL
   toDataUrl: async (text, opts = {}) => {
-    return await QRCode.toDataURL(String(text), { width: 320, margin: 1, ...opts });
+    return await QRCode.toDataURL(String(text), {
+      width: 320,
+      margin: 1,
+      ...opts,
+    });
   },
 
-  // mở link bằng default browser
+  // mở link default browser
   openExternal: async (url) => {
     return await ipcRenderer.invoke("open-external", String(url));
   },
 
-  // lưu PNG từ dataURL (data:image/png;base64,...)
+  // lưu PNG
   savePng: async ({ filename, dataUrl }) => {
     return await ipcRenderer.invoke("save-png", { filename, dataUrl });
   },
 
-  // lưu PDF từ base64 (data:application/pdf;base64,...)
-  savePdf: async ({ filename, dataUrl }) => {
-    return await ipcRenderer.invoke("save-pdf", { filename, dataUrl });
-  },
-
-  // (tuỳ bạn) call server nội bộ (nếu cần)
-  apiPost: async (path, body) => {
-    return await ipcRenderer.invoke("api-post", { path, body });
+  // lưu PDF (dùng printToPDF của Electron)
+  savePdf: async ({ filename }) => {
+    return await ipcRenderer.invoke("save-pdf", { filename });
   },
 });
