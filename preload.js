5d9ab147d3a8b11c0df5289a944142a5f42b582b
dieutrinh <56671269+dieutrinh@users.noreply.github.com>
2025-12-23 21:35:56 +0700

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,28 +1,27 @@
+// preload.js
+"use strict";
+
 const { contextBridge, ipcRenderer } = require("electron");
 const QRCode = require("qrcode");
 
 contextBridge.exposeInMainWorld("qrFactory", {
-  // tạo QR dataURL
   toDataUrl: async (text, opts = {}) => {
     return await QRCode.toDataURL(String(text), {
       width: 320,
       margin: 1,
-      ...opts,
+      ...opts
     });
   },
 
-  // mở link default browser
   openExternal: async (url) => {
     return await ipcRenderer.invoke("open-external", String(url));
   },
 
-  // lưu PNG
   savePng: async ({ filename, dataUrl }) => {
     return await ipcRenderer.invoke("save-png", { filename, dataUrl });
   },
 
-  // lưu PDF (dùng printToPDF của Electron)
   savePdf: async ({ filename }) => {
     return await ipcRenderer.invoke("save-pdf", { filename });
-  },
+  }
 });
