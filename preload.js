9dfca6431988518b291b4a18f7a4547f066b0e9f
dieutrinh <56671269+dieutrinh@users.noreply.github.com>
2025-12-23 21:52:00 +0700

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,4 +1,3 @@
-// preload.js
 "use strict";
 
 const { contextBridge, ipcRenderer } = require("electron");
@@ -6,21 +5,14 @@ const QRCode = require("qrcode");
 
 contextBridge.exposeInMainWorld("qrFactory", {
   toDataUrl: async (text, opts = {}) => {
-    return await QRCode.toDataURL(String(text), {
-      width: 320,
-      margin: 1,
-      ...opts
-    });
+    return await QRCode.toDataURL(String(text), { width: 320, margin: 1, ...opts });
   },
-
   openExternal: async (url) => {
     return await ipcRenderer.invoke("open-external", String(url));
   },
-
   savePng: async ({ filename, dataUrl }) => {
     return await ipcRenderer.invoke("save-png", { filename, dataUrl });
   },
-
   savePdf: async ({ filename }) => {
     return await ipcRenderer.invoke("save-pdf", { filename });
   }
